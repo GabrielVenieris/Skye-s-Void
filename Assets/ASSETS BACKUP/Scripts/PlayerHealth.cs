d1eac72f6df435738a8bfaa1f36439544ce1e465
KiryuKyosuke   Set status <pedrhaddad@gmail.com>
2023-08-11 14:54:21 -0300

    primeiro commit haddad

--- Assets/ASSETS BACKUP/Scripts/PlayerHealth.cs
+++ Assets/ASSETS BACKUP/Scripts/PlayerHealth.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 10;
+    public Healthbar healthBar;
+
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
    public void TakeDamage (int amount)
     {
         health -= amount;
+        healthBar.SetHealth(health);
         if(health <= 0)
         {
             Destroy(gameObject);
