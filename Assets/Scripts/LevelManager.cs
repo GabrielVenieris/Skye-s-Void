b572d563c55ff6b1ed97a5d336020ec2ffc017fe
Gabriel Venieris Machado <bielvenieris@gmail.com>
2023-09-29 14:29:21 -0300

    Atualização parcial LevelManager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-
+    public static LevelManager instance;
     public GameLevel currentLevel;
     public int kills = 0;
 
 
 
 
+    private GameLevel previousLevel; // Variável para armazenar o nível anterior
 
     public int enemiesPerLevel = 30;
     public int enemyHealth = 1;
@@ -20,6 +21,19 @@ public class LevelManager : MonoBehaviour
 
     public bool lastEnemyIsABoss = false;
 
+
+     void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else{
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -36,29 +50,55 @@ public class LevelManager : MonoBehaviour
 
     public void SetupLevelChanged()
     {
-        switch (currentLevel)
-        {
-            case GameLevel.Level1:
+         string sceneName = SceneManager.GetActiveScene().name;
+
+    switch (sceneName)
+    {
+        case "Mapa 1":
+            currentLevel = GameLevel.Level1;
             SetupLevel(2);
-                break; // Define a quantidade de inimigos para o Level 1
+            break;
 
-            case GameLevel.Level2:
+        case "Mapa 2":
+            currentLevel = GameLevel.Level2;
             SetupLevel(3);
-                break; // Define a quantidade de inimigos para o Level 2
+            break;
 
-            case GameLevel.Level3:
+        case "Mapa 3":
+            currentLevel = GameLevel.Level3;
             SetupLevel(4);
-                break; // Define a quantidade de inimigos para o Level 3
+            break;
 
-            // Adicione outros casos para outros níveis
-            default:
-                break; // Se o nível não for reconhecido, retorne 0 inimigos
-        }
+        default:
+            currentLevel = GameLevel.Level1; // Cena padrão
+            SetupLevel(1);
+            break;
+    }
+        // switch (currentLevel)
+        // {
+        //     case GameLevel.Level1:
+        //     SetupLevel(2);
+        //         break; // Define a quantidade de inimigos para o Level 1
+
+        //     case GameLevel.Level2:
+        //     SetupLevel(3);
+        //         break; // Define a quantidade de inimigos para o Level 2
+
+        //     case GameLevel.Level3:
+        //     SetupLevel(4);
+        //         break; // Define a quantidade de inimigos para o Level 3
+
+        //     // Adicione outros casos para outros níveis
+        //     default:
+        //         break; // Se o nível não for reconhecido, retorne 0 inimigos
+        // }
     }
 
 
      void CheckLevelCompleted()
 {
+    GameLevel current = currentLevel; // Armazene o nível atual antes da verificação
+
     EnemyGenerator enemyGenerator = FindObjectOfType<EnemyGenerator>();
     if (enemyGenerator != null)
     {
@@ -74,14 +114,18 @@ public class LevelManager : MonoBehaviour
             return; // Nível completo
         }
     }
-
+    if (current != currentLevel)
+    {
+        // A cena mudou, chame SetupLevelChanged
+        SetupLevelChanged();
+    }
     return; // Nível não completo
 }
 
 
 void SetupLevel(int level) {
         kills = 0;
-        // enemiesPerLevel = 1 * (1+ ((level-1)/10));
+        enemiesPerLevel = 1 * (1+ ((level-1)/10));
         enemyHealth     = 1 * (1 + ((level-1)/10));
         enemyDamage     = 1 * (1+ ((level-1)/10));
         lastEnemyIsABoss = (level == 3);
